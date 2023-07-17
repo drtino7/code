5a3a5f4b55b394507da71263de0ab7b5aac282cb
valentino grande <drtino777@gmail.com>
2023-07-17 23:55:40 +0000

    keys and values has been added

--- variables.py
+++ variables.py
@@ -3,5 +3,25 @@ string = "valentino"
 boolean = True
 
 my_list=[1,2,3]
-dictionary={'valentino':15,'juan':27}
+
+my_dict = {'apple': 1, 'banana': 2, 'orange': 3}
+
+
+#uses of functions#
+
+
+# Using keys()
+for key in my_dict.keys():
+    print(key)
+
+# Using values()
+for value in my_dict.values():
+    print(value)
+
+
+
+#keys:apple,banana,orange
+#values:1,2,3
+
+
 
