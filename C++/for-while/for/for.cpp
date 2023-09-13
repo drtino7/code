d57018bcb999856cc9f0e56e8dbd3f19cd02d776
valentino grande <valentinogrande972@gmail.com>
2023-09-13 18:14:11 +0000

    preetiering code :)

---
+++ C++/for-while/for/for.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+
+  for(int i = 0; i <= 10;i++){
+	  std::cout<<i;
+	  
+  }
+
+
+  return 0;
+}
---
+++ C++/for-while/while/while.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int a = 0;
+while(a <= 15){
+cout<<a<<" ";
+
+a++;
+}
+
+
+
+
+return 0;
+}
