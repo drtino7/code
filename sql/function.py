6fcbee60b4eea998498b1344d6dd890e550ec282
valentino grande <valentinogrande972@gmail.com>
2023-12-22 19:33:10 -0300

    deleted:    python/openbootcamp/gui/.main.py.swp new file:   sql/function.py

---
+++ sql/function.py
@@ -0,0 +1,22 @@
+import sqlite3
+import pandas
+
+conn = sqlite3.connect("database.db")
+
+square = lambda x: x * x
+
+conn.create_function("square", 1, square)
+
+cursor = conn.cursor()
+
+execution = cursor.execute("SELECT * FROM user")
+
+data = pandas.DataFrame(execution)
+
+cursor.close()
+conn.close()
+
+#with sqlite3.connect("database.db") as conn:
+#    cursor = conn.cursor()
+#    execution = cursor.execute("SELECT * FROM user")
+#    data = pandas.DataFrame(execution)
