b6d5a800273a505919af41cd6e8f59e42bd19d4c
drtino7 <87675718+drtino7@users.noreply.github.com>
2023-12-23 20:58:08 -0300

    added numpy

---
+++ python/numpy/numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+my_array = np.array([1,2,3,4,5,6,7])
+my_matriz = np.array([1,2,3],[4,5,6])
+my_matrix = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+
+print(my_matrix.ndim)
+print(my_array[0:2])
+
