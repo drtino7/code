a5aed005f968be277449d4478be64bbee1a503e1
valentino grande <valentinogrande972@gmail.com>
2023-12-27 14:39:46 -0300

    django

--- python/django/my_block/post/urls.py
+++ python/django/my_block/post/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test',views.test,name='test'),
+    path('queries/',views.queries,name='queries'),
+    path('update/',views.update,name="update"),
 ]
--- python/django/my_block/post/views.py
+++ python/django/my_block/post/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Entry,Author
 
 # Create your views here.
 
-def test(request):
-    return HttpResponse("test")
+def queries(request):
+    #get all
+    Authors = Author.objects.all()
+    #filter
+    Author_filteres = Author.objects.filter(email='nruiz@example.com')
+    #get one
+    author = Author.objects.get(id=1)
+    #limit
+    limit = Author.objects.all()[:10]
+    #get 5 values skiping 5 first
+    skip = Author.objects.all()[5:10]
+    #order by
+    Authors = Author.objects.all().order_by('email')
+    #
+    #__lte menor than equal
+    #__gte greater than equal
+    #__gt greater than
+    #__lt less than
+    #__contains contain
+    #__exact exact
+    filterer = Author.objects.filter(id__lte=15)
+    #author who has in his name the word yes
+    author_yes = Author.objects.filter(name__contains='yes')
+
+    return render(request,'post.html',{'Authors':Authors,'filter':Author_filteres,'aut':author,'limit':limit,'skip':skip})
+
+def update(request):
+    author = Author.objects.get(id=1)
+    author.name='juan'
+    author.email='j@j.com'
+    author.save()
+    return HttpResponse('updated')
